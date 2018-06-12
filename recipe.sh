e9c425ac57add6d2ea9d13aed10b9f13488a53bc
Corean Gonzales <coreangonzales@gmail.com>
2018-06-12 10:34:27 -0500

    recipe.sh

--- recipe.sh
+++ recipe.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-curl -X PUT -H "Content-Type: application/json" -d { "title":"this is a test", "time":"30", "desc":"this is a description", "ingredient": "ingredient", "instruction": "instruction" } \
+curl -X PUT -H "Content-Type: application/json" -d '{ 'title': "this is a test", 'time': "30", 'desc':"this is a description", 'ingredient': "ingredient", 'instruction': "instruction" }' \
   http://localhost:4000/recipe
